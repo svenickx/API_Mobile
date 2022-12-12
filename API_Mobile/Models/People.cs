74cee18cbc00570d8777fa16a8052d030c0b25c4
Sven Dockx <sven.dockx@gmail.com>
2022-12-12 11:23:28 +0100

    ok

--- API_Mobile/Models/People.cs
+++ API_Mobile/Models/People.cs
@@ -4,5 +4,7 @@
     {
         public int Id { get; set; }
         public string Name { get; set; } = String.Empty;
+        public string MainPicture { get; set; } = String.Empty;
+        public IEnumerable<string>? Pictures { get; set; }
     }
 }
--- API_Mobile/Program.cs
+++ API_Mobile/Program.cs
@@ -18,13 +18,27 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
 
-app.MapGet("/people", () =>
+app.MapGet("/people/{id}", (int page) =>
 {
+    int nb = (page - 1) * 5;
     String JSONtxt = File.ReadAllText(@".\files\json\people.json");
-    var people = JsonConvert.DeserializeObject<IEnumerable<People>>(JSONtxt);
-    return people;
+    List<People> people = JsonConvert.DeserializeObject<List<People>>(JSONtxt);
+    IEnumerable<People>? result = new List<People>();
+    if (nb > people.Count - 1)
+    {
+        return result;
+    }
+    
+    if (nb + 5 >= people.Count)
+    {
+        result = people.GetRange(nb, people.Count - nb - 1);
+    } 
+    else
+    {
+        result = people.GetRange(nb, 5);
+    }
+    return result;
 })
 .WithName("GetPeople");
 
